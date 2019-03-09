34f8a4447dd4f45d9deb42eb33f707c63a69a84b
Pandher <pandher.a@hotmail.com>
2019-03-09 02:15:39 -0500

    Completed lecture on outputting text

---
+++ BasicSyntax/OutputtingText.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Printing a flush line" << flush;
+    cout << "Hello My Name Is Amrit Pandher" << endl;
+    cout << "Banana " << "Apple " << "Orange " << "Pear " << endl;
+    return 0;
+}
+
